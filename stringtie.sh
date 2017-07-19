d303de93bb7d8ce4d542e1ee462dc2296f6d9644
DAMGoossens <dymphygoossens@gmail.com>
2017-07-19 15:28:51 +0200

    Add files via upload

--- stringtie.sh
+++ stringtie.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 #Msc thesis Dymphy Goossens
 
-#Assembly by stringtie
-progs/stringtie/stringtie stringtie/input/1657_A_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o stringtie/output/1657_A.gtf -p 8 -e -b stringtie/output/ballgown/1657_A
-progs/stringtie/stringtie stringtie/input/1657_B_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o stringtie/output/1657_B.gtf -p 8 -e -b stringtie/output/ballgown/1657_B
-progs/stringtie/stringtie stringtie/input/1657_C_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o stringtie/output/1657_C.gtf -p 8 -e -b stringtie/output/ballgown/1657_C
-progs/stringtie/stringtie stringtie/input/1657_M_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o stringtie/output/1657_M.gtf -p 8 -e -b stringtie/output/ballgown/1657_M
-progs/stringtie/stringtie stringtie/input/1657_N_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o stringtie/output/1657_N.gtf -p 8 -e -b stringtie/output/ballgown/1657_N
-progs/stringtie/stringtie stringtie/input/1657_O_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o stringtie/output/1657_O.gtf -p 8 -e -b stringtie/output/ballgown/1657_O
-printf "Stringtie has been run"
+#ST
+progs/stringtie/stringtie input/1657_A_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_A.gtf -p 8 -e -b o_stringtie/ballgown/1657_A
+progs/stringtie/stringtie input/1657_B_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_B.gtf -p 8 -e -b o_stringtie/ballgown/1657_B
+progs/stringtie/stringtie input/1657_C_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_C.gtf -p 8 -e -b o_stringtie/ballgown/1657_C
 
-#Mergen van samples
-#progs/stringtie/stringtie --merge -G Araport11_GFF3_genes_transposons.201606.gtf -o stringtie/output/merged.gtf -l BAY stringtie/output/1657_A.gtf stringtie/output/1657_B.gtf stringtie/output/1657_C.gtf stringtie/output/1657_M.gtf stringtie/output/1657_N.gtf stringtie/output/1657_O.gtf
-#printf "Samples have been merged"
+#LP
+progs/stringtie/stringtie input/1657_G_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_G.gtf -p 8 -e -b o_stringtie/ballgown/1657_G
+progs/stringtie/stringtie input/1657_H_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_H.gtf -p 8 -e -b o_stringtie/ballgown/1657_H
+progs/stringtie/stringtie input/1657_I_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_I.gtf -p 8 -e -b o_stringtie/ballgown/1657_I
+
+#HT
+progs/stringtie/stringtie input/1657_M_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_M.gtf -p 8 -e -b o_stringtie/ballgown/1657_M
+progs/stringtie/stringtie input/1657_N_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_N.gtf -p 8 -e -b o_stringtie/ballgown/1657_N
+progs/stringtie/stringtie input/1657_O_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_O.gtf -p 8 -e -b o_stringtie/ballgown/1657_O
+
+#HL
+progs/stringtie/stringtie input/1657_S_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_S.gtf -p 8 -e -b o_stringtie/ballgown/1657_S
+progs/stringtie/stringtie input/1657_T_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_T.gtf -p 8 -e -b o_stringtie/ballgown/1657_T
+progs/stringtie/stringtie input/1657_U_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_U.gtf -p 8 -e -b o_stringtie/ballgown/1657_U
