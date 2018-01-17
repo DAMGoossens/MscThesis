9e67511867deaa8e803462826706a076854456d4
DAMGoossens <dymphygoossens@gmail.com>
2018-01-17 15:45:53 +0100

    Delete bam_2_sortedbam.sh

--- bam_2_sortedbam.sh
+++
@@ -1,6 +0,0 @@
-progs/samtools/samtools sort -o stringtie/input/1657_A_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_A.bam 
-progs/samtools/samtools sort -o stringtie/input/1657_B_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_B.bam 
-progs/samtools/samtools sort -o stringtie/input/1657_C_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_C.bam 
-progs/samtools/samtools sort -o stringtie/input/1657_M_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_M.bam 
-progs/samtools/samtools sort -o stringtie/input/1657_N_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_N.bam 
-progs/samtools/samtools sort -o stringtie/input/1657_O_sorted.bam /mnt/scratch/nijve002/At_WL_nobackup/hisat2/1657_O.bam
