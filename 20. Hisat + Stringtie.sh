395a578bc7c6efe7cab39761aa64cb7ae495a65c
DAMGoossens <dymphygoossens@gmail.com>
2018-01-17 15:50:39 +0100

    Update and rename hisat.sh to 20. Hisat + Stringtie.sh

--- 20. Hisat + Stringtie.sh
+++ 20. Hisat + Stringtie.sh
@@ -83,4 +83,19 @@ progs/samtools/samtools index -b input/1657_T_sorted.bam
 progs/samtools/samtools index -b input/1657_U_sorted.bam
 printf "Indexing bam files done"
 
+#Running stringtie
+progs/stringtie/stringtie input/1657_A_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_A.gtf -p 8 -e -b o_stringtie/ballgown/1657_A
+progs/stringtie/stringtie input/1657_B_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_B.gtf -p 8 -e -b o_stringtie/ballgown/1657_B
+progs/stringtie/stringtie input/1657_C_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_ST -o o_stringtie/1657_C.gtf -p 8 -e -b o_stringtie/ballgown/1657_C
+progs/stringtie/stringtie input/1657_G_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_G.gtf -p 8 -e -b o_stringtie/ballgown/1657_G
+progs/stringtie/stringtie input/1657_H_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_H.gtf -p 8 -e -b o_stringtie/ballgown/1657_H
+progs/stringtie/stringtie input/1657_I_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_LP -o o_stringtie/1657_I.gtf -p 8 -e -b o_stringtie/ballgown/1657_I
+progs/stringtie/stringtie input/1657_M_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_M.gtf -p 8 -e -b o_stringtie/ballgown/1657_M
+progs/stringtie/stringtie input/1657_N_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_N.gtf -p 8 -e -b o_stringtie/ballgown/1657_N
+progs/stringtie/stringtie input/1657_O_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HT -o o_stringtie/1657_O.gtf -p 8 -e -b o_stringtie/ballgown/1657_O
+progs/stringtie/stringtie input/1657_S_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_S.gtf -p 8 -e -b o_stringtie/ballgown/1657_S
+progs/stringtie/stringtie input/1657_T_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_T.gtf -p 8 -e -b o_stringtie/ballgown/1657_T
+progs/stringtie/stringtie input/1657_U_sorted.bam -G Araport11_GFF3_genes_transposons.201606.gtf -l BAY_HL -o o_stringtie/1657_U.gtf -p 8 -e -b o_stringtie/ballgown/1657_U
+print "Running stringtie done"
+
 printf "End of script"
